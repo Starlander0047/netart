3c08bedf21049ca2da0c0e09d500ff0e6dc85027
Starlander0047 <asad.alei0047@gmail.com>
2024-02-27 23:37:06 +0530

    Final commit 2

--- src/components/Mainpage.js
+++ src/components/Mainpage.js
@@ -21,7 +21,7 @@ function Mainpage() {
         </div>
         <div className="rightPart">
             <b id='boldMargin' >C.R.I. PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th time.</b>
-            <div>
+            <div style={{marginLeft: "25px"}}>
                 <p> &bull; C.R.I.'s energy efficient products are well recognized by various Government Institutions, as trustworthy products for various projects across the globe to save energy.</p>
                 <p> &bull; C.R.I. is the highest contributor in the country for the projects of EESL (Energy Efficiency Services Limited) to replace the old inefficient pumps with 5 Star rated energy efficient smart pumps with IoT enabled control panel. </p>
             </div>
